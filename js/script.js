95499445aded0d85d1299e53365494d3f3c37c8a
Gianpaolo <pao.barga@gmail.com>
2021-01-22 17:33:03 +0100

    commit iniziale: esercizio base fatto

---
+++ js/script.js
@@ -0,0 +1,240 @@
+//DATI
+const fieldCodes = [
+  'W', 'U', 'B', 'R', 'G'
+]
+
+const cardTypes = [
+  'terre',
+  'creature',
+  'incantesimi',
+  'artefatti',
+  'instantanei',
+  'stregonerie'
+]
+
+const powerValues = [1, 2, 3, 4, 5];
+
+// Abbiamo creato un oggetto di oggetti, come riferimento
+// di una edizione. Se ad esempio scrivo editions['SP']
+// allora otterrò tutto un oggetto che descrive
+// con più dettagli l'edizione.
+// come oggetto di oggetti, può essere navigato solo con il for-in
+const editions = {
+
+  'BL': {
+      edition: 'Boolean',
+      rarity: 'blue'
+  },
+
+  'SP': {
+      edition: 'Special',
+      rarity: 'red'
+  }
+
+}
+
+const cards = [{
+
+  cardName: 'Grizzly Bears',
+
+  cost: {
+    genericCostNumber: 1,
+    costFields: [ // colors array con riferimento a fieldCodes
+      fieldCodes[0],  // 'W',  - un suo riferimento
+      fieldCodes[2]   // 'B'
+    ],
+  },
+
+  picture: 'images/i.png',
+  cardType: cardTypes[1],
+  cardObject: 'Bear',
+
+  editionType: editions['BL'],
+
+  description: 'Lorem ipsum',
+  story: 'Naltro Lorem Ipsum',
+
+  score: {
+    power: 2,  // filtrarlo per power
+    toughness: 2
+  }
+
+  },
+  {
+
+    cardName: 'Sviluppatore guerriero',
+
+    cost: {
+      genericCostNumber: 3,
+      costFields: [ // colors array con riferimento a fieldCodes
+        fieldCodes[2],
+        fieldCodes[3]
+      ],
+    },
+
+    picture: 'images/g.png',  // da inserire immagine
+    cardType: cardTypes[1],
+    cardObject: 'Bear',
+
+    editionType: editions['BL'],
+
+    description: 'Lo sviluppatore guerriero spezza i byte in bit!',
+    story: 'Lo sviluppatore guerriero è una forma di essere umano evoluto.',
+
+    score: {
+      power: 5,  // r
+      toughness: 3
+    }
+
+    },
+  {
+
+    cardName: 'Sviluppatore junior',
+
+    cost: {
+      genericCostNumber: 2,
+      costFields: [ // colors array con riferimento a fieldCodes
+        fieldCodes[1],
+        fieldCodes[3]
+      ],
+    },
+
+    picture: 'images/g.png',  // da inserire immagine
+    cardType: cardTypes[1],
+    cardObject: 'Pokemon',
+
+    editionType: editions['SP'],
+
+    description: 'Lo sviluppatore junior spezza i byte in bit!',
+    story: 'Lo sviluppatore junior è una forma di essere umano evoluto.',
+
+    score: {
+      power: 4,  // r
+      toughness: 5
+    }
+
+    },
+  {
+
+    cardName: 'Sviluppatore mid-level',
+
+    cost: {
+      genericCostNumber: 3,
+      costFields: [ // colors array con riferimento a fieldCodes
+        fieldCodes[1],
+        fieldCodes[2],
+        fieldCodes[3],
+      ],
+    },
+
+    picture: 'images/g.png',  // da inserire immagine
+    cardType: cardTypes[1],
+    cardObject: 'Enchant',
+
+    editionType: editions['BL'],
+
+    description: 'Lo sviluppatore guerriero spezza i byte in bit!',
+    story: 'Lo sviluppatore guerriero è una forma di essere umano evoluto.',
+
+    score: {
+      power: 5,  // r
+      toughness: 3
+    }
+
+    },
+  {
+
+    cardName: 'Sviluppatore senior',
+
+    cost: {
+      genericCostNumber: 3,
+      costFields: [ // colors array con riferimento a fieldCodes
+        fieldCodes[2],
+        fieldCodes[3]
+      ],
+    },
+
+    picture: 'images/g.png',  // da inserire immagine
+    cardType: cardTypes[1],
+    cardObject: 'Bear',
+
+    editionType: editions['BL'],
+
+    description: 'Lo sviluppatore guerriero spezza i byte in bit!',
+    story: 'Lo sviluppatore guerriero è una forma di essere umano evoluto.',
+
+    score: {
+      power: 5,  // r
+      toughness: 3
+    }
+
+    },
+]
+
+// LOGICA
+//creiamo una funz che filtra l'array a seconda del valore di power
+function filterByPower(arr, powerValue){
+  return arr.filter(el => el.score.power === powerValue);
+};
+
+//creiamo una funz che filtra l'array a seconda del valore di card-type
+function filteredByCardType(arr, cardTypeValue){
+  return arr.filter(el => el.cardType === cardTypevalue);
+};
+
+// console.log(filterByPower(cards, 5))
+
+//funzione per renderizzare le carte
+function renderCards(domTarget, array) {
+
+  let content = "";
+  
+  if (array.length === 0) {
+    content = "<div>Non ci sono carte corrispondenti al valore selezionato.</div>";
+  } else {
+    array.forEach(el => {
+      content += `
+        <div class="card">
+        <h1>${el.cardName}</h1>
+        </div>
+      `
+    })
+  }
+  
+  domTarget.innerHTML = content;
+}
+
+//funzione per renderizzare le option in select
+function renderSelect(domTarget, array) {
+  let content = "";
+  
+  array.forEach(el => {
+    content += `<option value="${el}">${el}</option>`;
+  })
+  
+  domTarget.innerHTML += content;
+}
+
+// OUTPUT
+//rendering iniziale dell'array completo: quello che vediamo al caricamento
+const cardsContainer = document.querySelector(".cards-container"); //definizione del target
+renderCards(cardsContainer, cards);
+
+//rendering della select in base ai valori di select disponibili decis in powerValues
+const cardsSelector = document.querySelector("#cards-selection"); //definizione del target
+renderSelect(cardsSelector, powerValues);
+
+//selezione power
+$("#cards-selection").change(function() {
+  if(isNaN($(this).val())) {
+    renderCards(cardsContainer, cards)
+    
+  } else {
+    const selectedValue = parseInt($(this).val());
+    const filteredArray = filterByPower(cards, selectedValue);
+
+    renderCards(cardsContainer, filteredArray)
+  }
+})
+
+
